46ac716ad0035e5818d32a0f70ca4329594ec879
GulajavaMinistudio <gulajava.mini@gmail.com>
2021-03-01 15:25:18 +0700

    Programmatic navigation dalam vue js App Vue

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,31 @@
         <router-link :to="{ name: 'JobList' }">List Jobs</router-link> |
         <router-link :to="{ name: 'About' }">About</router-link>
     </div>
+
+    <!-- Navigasi dengan programmatically dengan router -->
+    <button @click="redirectPage" class="button-nav">Redirect</button>
+    <button @click="gobackPage" class="button-nav">Go Back</button>
+    <button @click="forwardPage" class="button-nav">Go Forward</button>
+
     <router-view />
 </template>
+
+<script>
+export default {
+    name: 'AppComponent',
+    methods: {
+        redirectPage() {
+            this.$router.push({ name: 'Homepage' });
+        },
+        gobackPage() {
+            this.$router.go(-1);
+        },
+        forwardPage() {
+            this.$router.go(1);
+        },
+    },
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -31,5 +54,12 @@
             background: crimson;
         }
     }
+}
+
+.button-nav {
+    margin: 0 10px;
+    padding: 10px;
+    border: none;
+    border-radius: 4px;
 }
 </style>
